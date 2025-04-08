32bc1a010273d916ff2792071e6cb0246c34a544
Alejandro <alejandrogonzalezaldeguer@gmail.com>
2025-04-08 12:21:34 +0200

    commit alejandro

--- Futbol/Program.cs
+++ Futbol/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hola soy Izan");
+            Console.WriteLine("Hola soy Alejandro");
         }
     }
 }
