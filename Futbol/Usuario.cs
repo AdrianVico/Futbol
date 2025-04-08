8773de9c674f3caf100cb5d456df10d98c343727
Alejandro <alejandrogonzalezaldeguer@gmail.com>
2025-04-08 12:31:03 +0200

    Clase Usuario

---
+++ Futbol/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futbol
+{
+    internal class Usuario
+    {
+        string nombre;
+        string password;
+        Plantilla plantilla;
+        public Usuario(string nombre, string password)
+        {
+            this.nombre = nombre;
+            this.password = password;
+            plantilla = new Plantilla(nombre);
+        }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Password { get => password; set => password = value; }
+        public Plantilla Plantilla { get => plantilla; set => plantilla = value; }
+
+        public override string ToString()
+        {
+            return $"Nombre: {nombre}, Password: {password}, Plantilla: {plantilla.Nombre}";
+        }
+
+    }
+}
