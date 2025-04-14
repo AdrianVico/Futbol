b79c06ded574bbee53976646b00eaf8bbc423e74
AdrianVicoUsuario <adrviclop@alu.edu.gva.es>
2025-04-14 12:50:58 +0200

    Modificación de la clase principal para las opciones de inicio de sesión

--- Futbol/Program.cs
+++ Futbol/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
 
 namespace Futbol
 {
@@ -7,18 +8,40 @@ namespace Futbol
 
         public static void InicioSesion()
         {
-            
+            Console.Write("");
         }
 
+        public static int Seleccion()
+        {
+            Console.Write("¿Tienes una cuenta creada?");
+            string[] opciones = { "Si", "No" };
+            Console.SetCursorPosition(Console.WindowWidth / 2, 0);
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("> "+opciones[0]);
+            Console.ResetColor();
+            Console.SetCursorPosition(Console.WindowWidth / 2, 1);
+            Console.WriteLine(opciones[1]);
+            ConsoleKeyInfo keyInfo;
+            int indice = 0;
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        //indice == -1 ? 0 : indice -1; 
+                        
+                        break;
+                }
+            }
+
+
+
+            return 0;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hola soy Izan");
-            Console.WriteLine("Hola soy Alejandro");
-            Console.WriteLine("Hola soy Sergio");
-            Console.WriteLine("Hola soy Marcos");
-            Console.WriteLine("Hola soy Marcos");
-            Console.WriteLine("Hola soy Marcos");
+            
         }
     }
 }
