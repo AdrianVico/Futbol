0585004ddfd56944a019af57ffe1e28cc16f6c3f
SergioAbadOrdonez <sergioabaddaw@gmail.com>
2025-04-14 12:00:04 +0200

    Creacion Mercado

--- Futbol/Mercado.cs
+++ Futbol/Mercado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,29 +9,57 @@ namespace Futbol
 {
     internal class Mercado
     {
-        List<Jugador> jugadoresEnMercado;
-        List<Equipo> equipos;
-        public Mercado()
+        List<Jugador> jugadoresMercado;
+        Equipo equipoUsu;
+
+        public Mercado(Equipo equipoUsu)
         {
-            jugadoresEnMercado = new List<Jugador>();
-            equipos = new List<Equipo>();
+            jugadoresMercado = new List<Jugador>();
+            this.equipoUsu = equipoUsu;
         }
         public void AddJugador(Jugador jugador)
         {
-            jugadoresEnMercado.Add(jugador);
+            jugadoresMercado.Add(jugador);
         }
-        public void VenderJugador(Jugador jugador, Equipo equipoDestino)
+        public void SeleccionarJugador()
         {
-            if (jugadoresEnMercado.Contains(jugador) && equipos.Contains(equipoDestino))
+            ConsoleKeyInfo keyInfo;
+            int indice = 0;
+            do
             {
-                Console.WriteLine($"Vendido {jugador.Nombre} a {equipoDestino.Nombre}");
-                jugadoresEnMercado.Remove(jugador);
-                equipoDestino.AddJugador(jugador);
-            }
-            else
-            {
-                Console.WriteLine("El jugador o el equipo no están disponibles.");
-            }
+                Console.WriteLine("Jugadores en el mercado:");
+                for (int i = 0; i < jugadoresMercado.Count; i++)
+                {
+                    if (i == indice)
+                    {
+                        Console.WriteLine($"> {jugadoresMercado[i].ToString()}");
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.WriteLine(jugadoresMercado[i].ToString());
+                    }
+                }
+                keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    indice = (indice == 0) ? jugadoresMercado.Count - 1 : indice - 1;
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    indice = (indice == jugadoresMercado.Count - 1) ? 0 : indice + 1;
+                }
+                Console.Clear();
+            } while (keyInfo.Key != ConsoleKey.Enter);
+
+            ComprarJugador(jugadoresMercado[indice]);
+        }
+        public void ComprarJugador(Jugador jugador)
+        {
+            Console.WriteLine($"Has comprado a {jugador.Nombre} por {jugador.Precio}.");
+            equipoUsu.AddJugador(jugador);
+            jugadoresMercado.Remove(jugador);
         }
     }
 }
