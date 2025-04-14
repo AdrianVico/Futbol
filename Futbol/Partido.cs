4efab8dd6c9c4a9eb4323ec7a24c1ca2a07b73d9
Izan <izajimmoy@alu.edu.gva.es>
2025-04-14 12:36:58 +0200

    Clase Jornada eliminada y fusionada con la clase Partido. Clase Partido optimizada y metodos agregados

--- Futbol/Partido.cs
+++ Futbol/Partido.cs
@@ -8,19 +8,54 @@ namespace Futbol
 {
     internal class Partido
     {
-        List<Equipo> equipos;
         string resultado;
-        public Partido(string resultado)
+        int numeroJornada;
+        List<Equipo> equipos;
+        Dictionary<Equipo, Equipo> partidos;
+        public Partido()
         {
-            this.resultado = resultado;
             equipos = new List<Equipo>();
+            partidos = new Dictionary<Equipo, Equipo>();
         }
         public string Resultado { get => resultado; set => resultado = value; }
-        
-        public override string ToString()
+
+        public string SimularResultado()
         {
-            string equiposString = string.Join(", ", equipos.Select(e => e.Nombre));
-            return $"Resultado: {resultado}, Equipos: {equiposString}";
+            Random golesEquipo1 = new Random();
+            golesEquipo1.Next(0, 9);
+            Random golesEquipo2 = new Random();
+            golesEquipo2.Next(0, 9);
+
+            resultado = $" {golesEquipo1} - {golesEquipo2} ";
+
+            return resultado;
+        }
+        public void AnyadirPartido()
+        {
+            for (int i = 0; i < equipos.Count - 1; i += 2)
+            {
+                Equipo local = equipos[i];
+                Equipo visitante = equipos[i + 1];
+
+                partidos.Add(local, visitante);
+            }
+        }
+
+        public void AumentarJornada()
+        {
+            numeroJornada += 1;
+        }
+
+        public void MostrarPartidos()
+        {
+            AumentarJornada();
+            Console.SetCursorPosition(Console.WindowWidth / 2, 0);
+            Console.WriteLine($"Partidos de la J{numeroJornada}");
+            Console.WriteLine();
+            foreach (KeyValuePair<Equipo, Equipo> e in partidos)
+            {
+                Console.WriteLine(e.Key + SimularResultado() + e.Value);
+            }
         }
     }
 }
