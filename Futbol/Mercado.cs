d3a43150571ae1c0fabe52c03120421fea344da1
Alejandro <alejandrogonzalezaldeguer@gmail.com>
2025-04-15 08:47:44 +0200

    dos metodos añadidos a mercado

--- Futbol/Mercado.cs
+++ Futbol/Mercado.cs
@@ -11,22 +11,36 @@ namespace Futbol
     {
         List<Jugador> jugadoresMercado;
         Equipo equipoUsu;
-        List<Jugador> JugadoresTotales;
 
         public Mercado(Equipo equipoUsu)
         {
             this.equipoUsu = equipoUsu;
         }
-        public void LeerFicheroJugadores()
+        public List<Jugador> LeerFicheroJugadores()
         {
+            List<Jugador> jugadoresTotales = new List<Jugador>();
             string[] jugadores = File.ReadAllLines("LEYENDAS.txt");
             foreach (string linea in jugadores)
             {
                 string[] datos = linea.Split(';');
                 Jugador jugador = new Jugador(datos[0], datos[1], Convert.ToInt32(datos[2]), datos[3]);
-                JugadoresTotales.Add(jugador);
+                jugadoresTotales.Add(jugador);
+            }
+            return jugadoresTotales;
+        }
+        public void AgregarJugadorMercado()
+        {
+            jugadoresMercado.Clear();
+            List <Jugador> jugadoresTotales = LeerFicheroJugadores();
+            Random random = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                int num = random.Next(0, jugadoresTotales.Count);
+                jugadoresMercado.Add(jugadoresTotales[num]);
+                jugadoresTotales.RemoveAt(num);
             }
         }
+
         public void SeleccionarJugador()
         {
             ConsoleKeyInfo keyInfo;
