38b036ee55704635a860a09f432bf17bf868d537
Izan <izajimmoy@alu.edu.gva.es>
2025-05-06 19:04:06 +0200

    Clase Jornada reincorporada al proyecto. Metodos actualizados

---
+++ Futbol/Jornada.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futbol
+{
+    internal class Jornada
+    {
+        List<Partido> partidos;
+        int numeroJornada;
+        public Jornada()
+        {
+            partidos = new List<Partido>();
+            numeroJornada = 1;
+        }
+
+        public void AddPartido(Partido partido)
+        {
+            partidos.Add(partido);
+        }
+        public void MostrarPartidos()
+        {
+            Console.SetCursorPosition(Console.WindowWidth / 2, 0);
+            Console.WriteLine($"Partidos de la J{numeroJornada}");
+            Console.WriteLine();
+            foreach (Partido partido in partidos)
+            {
+                Console.WriteLine(partido.ToString());
+            }
+            numeroJornada++;
+        }
+    }
+}
