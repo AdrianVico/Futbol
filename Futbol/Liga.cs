d71e297803f11c7ae23f35836beb305527890118
Izan <izajimmoy@alu.edu.gva.es>
2025-05-06 19:05:45 +0200

    Clase Liga actuliazada. Metodos actualizados. Faltan por terminar algunos

--- Futbol/Liga.cs
+++ Futbol/Liga.cs
@@ -8,43 +8,130 @@ namespace Futbol
 {
     internal class Liga
     {
-        string nombreLiga;
-        List<Equipo> equipos;
-        public Liga(string nombreLiga)
+        Dictionary<Equipo, List<string>> resultados;
+        Dictionary<Equipo, int> puntos;
+        int totalJornadas;
+        string archivo = "jornadas.txt";
+
+        public Liga()
         {
-            this.nombreLiga = nombreLiga;
-            equipos = new List<Equipo>();
+            resultados = new Dictionary<Equipo, List<string>>();
+            puntos = new Dictionary<Equipo, int>();
+            totalJornadas = 0;
         }
-        public string Nombre { get => nombreLiga; set => nombreLiga = value; }
-        public void AddEquipo(Equipo equipo)
+
+        public void CargarResultados()
         {
-            equipos.Add(equipo);
-        }
-        public void MostrarEquipos()
-        {
-            Console.WriteLine($"Equipos de la liga {nombreLiga}:");
-            foreach (Equipo equipo in equipos)
+            if (!File.Exists(archivo))
             {
-                Console.WriteLine(equipo.Nombre);
+                Console.WriteLine("El archivo no se encontró.");
+                return;
             }
+
+            string[] lineas = File.ReadAllLines(archivo);
+            int jornadaActual = 1;
+
+            foreach (string linea in lineas)
+            {
+                if (linea.StartsWith("-"))
+                {
+                    jornadaActual++;
+                }
+                else
+                {
+                    string[] datos = linea.Split(';');
+                    string nombreEquipo = datos[0];
+                    string resultado = datos[1];
+                    /*
+                    if (!resultados.ContainsKey(nombreEquipo))
+                    {
+                        resultados[nombreEquipo] = new List<string>();
+                        puntos[nombreEquipo] = 0;
+                    }
+
+                    resultados[nombreEquipo].Add(resultado);
+
+                    string[] goles = resultado.Split('-');
+                    int golesFavor = int.Parse(goles[0]);
+                    int golesContra = int.Parse(goles[1]);
+
+                    if (golesFavor > golesContra)
+                        puntos[nombreEquipo] += 3;
+                    else if (golesFavor == golesContra)
+                        puntos[nombreEquipo] += 1;*/
+                }
+            }
+
+            totalJornadas = CalcularTotalJornadas();
+        }
+
+        private int CalcularTotalJornadas()
+        {
+            int jornadas = 1;
+            string[] lineas = File.ReadAllLines(archivo);
+
+            foreach (string linea in lineas)
+            {
+                if (linea.StartsWith("-"))
+                    jornadas++;
+            }
+
+            return jornadas;
         }
 
         public void MostrarClasificacion()
         {
-            Console.SetCursorPosition(Console.WindowWidth / 2, 0);
-            Console.WriteLine(nombreLiga);
-            Console.WriteLine();
-            foreach (Equipo equipo in equipos)
+            CargarResultados();
+            Console.WriteLine("\nCLASIFICACIÓN GENERAL\n");
+            Console.Write("Equipo".PadRight(15));
+
+            for (int j = 1; j <= totalJornadas; j++)
+                Console.Write($"J{j}   ");
+
+            Console.WriteLine("PT");
+            /*
+            foreach (var equipo in resultados)
             {
-                Console.WriteLine($"{equipo}");
-            }
+                string nombre = equipo.Key;
+                List<string> res = equipo.Value;
+
+                Console.Write(nombre.PadRight(15));
+
+                foreach (string r in res)
+                    Console.Write($"{r.PadRight(5)}");
+
+                for (int i = res.Count; i < totalJornadas; i++)
+                    Console.Write("---- ");
+
+                Console.WriteLine($"{puntos[nombre]}");
+            }*/
         }
 
-        public override string ToString()
+        public void MostrarJornadas()
         {
-            string equiposString = string.Join(", ", equipos.Select(e => e.Nombre));
-            
-            return $"Nombre: {nombreLiga}, Equipos: {equiposString}";
+            if (!File.Exists(archivo))
+            {
+                Console.WriteLine("El archivo no se encontró.");
+                return;
+            }
+
+            string[] lineas = File.ReadAllLines(archivo);
+            int jornadaActual = 1;
+
+            Console.WriteLine("\nRESULTADOS POR JORNADA:\n");
+
+            foreach (string linea in lineas)
+            {
+                if (linea.StartsWith("-"))
+                {
+                    jornadaActual++;
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine($"J{jornadaActual}: {linea}");
+                }
+            }
         }
     }
 }
